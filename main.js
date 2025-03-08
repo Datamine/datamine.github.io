3a7d24fa8b5b7e15f21500ce7111925710093309
datamine <contact@johnloeber.com>
2025-03-08 11:33:23 -0800

    added twitter image

--- main.js
+++ main.js
@@ -11,22 +11,23 @@ document.addEventListener('DOMContentLoaded', function() {
   const images = [
     'images/beach.jpg',
     'images/self2.jpg',
-    'images/self.jpg'
+    'images/self.jpg',
+    'images/twitter-img.jpg'
   ];
-  
+
   let currentIndex = 0;
   const profileImg = document.querySelector('.float-right img');
-  
+
   if (profileImg) {
     // Set initial image
     profileImg.src = images[currentIndex];
-    
+
     // Set up click handler for the image
     profileImg.addEventListener('click', function() {
       currentIndex = (currentIndex + 1) % images.length;
       profileImg.src = images[currentIndex];
     });
-    
+
     // Change the cursor to indicate it's clickable
     profileImg.style.cursor = 'pointer';
   }
