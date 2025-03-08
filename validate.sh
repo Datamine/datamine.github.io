c8709a8e9fe6b7ac45ece884fa463f2531b8736f
datamine <contact@johnloeber.com>
2025-03-08 11:58:57 -0800

    add various validators

--- validate.sh
+++ validate.sh
@@ -1,4 +1,10 @@
-echo "HTML Validator"
+echo ">>>> HTML Validator"
 html-validator --file index.html --verbose
-echo "HTML Hint"
-htmlhint index.html
+# echo "HTML Hint"
+# htmlhint index.html
+echo ""
+echo ">>>> Selector check"
+python3 check_selectors.py
+echo ""
+echo ">>>> Check links are not broken"
+linkchecker index.html
